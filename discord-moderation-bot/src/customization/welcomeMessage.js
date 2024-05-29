511c9b37c94c7ba4e6e35b17d166ecac78f17be2
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 22:23:12 +0530

    Added: `discord-moderation-bot/src/customization/welcomeMessage.js`

---
+++ discord-moderation-bot/src/customization/welcomeMessage.js
@@ -0,0 +1,8 @@
+const welcomeMessage = {
+  sendWelcomeMessage: (user) => {
+    // Logic to send a welcome message to the user
+    console.log(`Welcome to the server, ${user}! We're glad to have you here.`);
+  }
+};
+
+export default welcomeMessage;
