3dfa1e7d675c5ace907407e558028029e822572c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 22:23:21 +0530

    Added: `discord-moderation-bot/src/dashboard/settings.js`

---
+++ discord-moderation-bot/src/dashboard/settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  prefix: '!',
+  moderationRoles: ['Moderator', 'Admin'],
+  maxWarnings: 3,
+  autoKickThreshold: 5,
+  autoBanThreshold: 10,
+  logChannel: 'moderation-log',
+  welcomeChannel: 'welcome',
+  defaultLanguage: 'en',
+  defaultTimezone: 'UTC',
+};
+
+module.exports = settings;
