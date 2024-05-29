cae3f447ac364b2307af4fb1b50f126bac4f4358
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 22:23:08 +0530

    Added: `discord-moderation-bot/src/customization/commandCustomization.js`

---
+++ discord-moderation-bot/src/customization/commandCustomization.js
@@ -0,0 +1,16 @@
+const commandCustomization = {
+  customizeCommand: (commandName, customizationOptions) => {
+    // Customization logic for commands
+  },
+  setResponseMessage: (commandName, responseMessage) => {
+    // Logic to set response message for a command
+  },
+  setCommandCooldown: (commandName, cooldownTime) => {
+    // Logic to set cooldown time for a command
+  },
+  setPermissionLevel: (commandName, permissionLevel) => {
+    // Logic to set permission level for a command
+  },
+};
+
+module.exports = commandCustomization;
